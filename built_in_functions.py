39f83ec21dc03dfb4f9607bbafef85244e52d819
Julián Koroluk <julian.koroluk@outlook.com>
2021-06-11 14:55:06 -0300

    Update built_in_functions.py
    print() y format()

--- built_in_functions.py
+++ built_in_functions.py
@@ -1,6 +1,19 @@
 # Built-in Functions
 # Funciones nativas de Python
 
+# print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
+# -> Imrime en pantalla/archivo un objeto, que puede separarse según 'sep' y una
+#    terminación según 'end'. En file, debe ir el puntero/variable de archivo a escribir.
+#    Todos los argumentos que no sean palabras claves (non-keyboard) son convertidos a str().
+#    'sep' y 'end' deben ser argumentos tipo string, o directamente no usarse (None)
+#    Si no se pasan objetos, solo imprime 'end'.
+
+print('¡Hola Mundo!') # Imprime un string
+entero = 128
+print('Numero entero: ', entero) # Imprime un string seguido de otro objeto int
+print('Números: ', 1, 2, 3, 4, 5, sep=';', end='\n\n') # Imprime un string, varios obejtos
+                                                    # separados por ';' y al final un salto de línea.
+
 # abs(x) -> devuelve el valor absoluto de un número (Módulo / Norma / Valor positivo)
 num_float = -5.25
 modulo = abs(num_float)
@@ -39,8 +52,15 @@ else:
 
 # ascii(object) ->
 
+# format(value, format_spec) -> Convierte un valor (x) en un formato específico (format_spec)
+# Existe una sintaxis de formateos estándar:
+# https://docs.python.org/3.7/library/string.html#formatspec
+# El formateo dpor defecto de format_spec es un string vacio que resulta el mismo efecto
+# de llamar a str(value)
+hexa = format(entero, 'x') # Convierte un entero a base hexadecimal
+print(f'El nro. {entero} en hexadecimal es: {hexa}\n')
+
 # bin(x) -> Convierte a un entero en binario, prefijado con "0b"
-entero = 128
 binario = bin(entero)
 print(f'{entero} en bineario es: {binario}')
 # Otra opción para convertir y sacandole el prefijo "0b":
@@ -48,3 +68,5 @@ binario = format(entero, 'b')
 print(f'Binario sin prefijo: {binario}')
 # ó directamente imprimiendo:
 print(f'Binario sin prefijo 2: {entero:b}')
+
+# class bool([x]) -> 
